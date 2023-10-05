bc2dc6544c7293740c2909be495b317a5d495739
Ruslan <ruslanchikbest@gmail.com>
2023-10-05 14:39:02 +0300

    Installing TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './scss/App.scss';
 import Users from './components/Users/Users';
 
 
-const App = () => {
+const App:React.FC = () => {
   return (
     <div className='wrapper'>
       <div className='users'>
